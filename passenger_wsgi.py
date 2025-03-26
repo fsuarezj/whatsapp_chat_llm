e3dc20ae3cf3f9594c5a1e746f5b26b4d712523e
Fernando Suárez Jiménez <fsuarezj@gmail.com>
2025-03-26 18:28:52 +0300

    Included virtual environment

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,11 +1,11 @@
 from src.app import app as application
 
-# Uncomment if using virtual environment
-# import sys
-# import os
-# INTERP = os.path.join(os.environ['HOME'], 'venv/bin/python')
-# if sys.executable != INTERP:
-#     os.execl(INTERP, INTERP, *sys.argv)
+Uncomment if using virtual environment
+import sys
+import os
+INTERP = os.path.join(os.environ['HOME'], '.venv/bin/python')
+if sys.executable != INTERP:
+    os.execl(INTERP, INTERP, *sys.argv)
 
 if __name__ == '__main__':
     application.run() 
