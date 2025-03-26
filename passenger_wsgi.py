ffd65872dab937be0a8adcbc4144971f371c21d2
Fernando Suárez Jiménez <fsuarezj@gmail.com>
2025-03-26 21:38:40 +0300

    Fixing bugs for deployment

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,8 +1,12 @@
 from src.app import app as application
+from dotenv import load_dotenv
 
 import sys
 import os
-INTERP = os.path.join(os.environ['HOME'], '.venv/bin/python')
+
+load_dotenv()
+print(os.getenv('HOME'))
+INTERP = os.path.join(os.getenv('HOME'), 'whatsapp-chat-llm.xastring.com/.venv/bin/python')
 if sys.executable != INTERP:
     os.execl(INTERP, INTERP, *sys.argv)
 
