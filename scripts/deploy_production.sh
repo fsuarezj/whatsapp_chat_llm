4234132e2dae5431acc272b3b6f1c6b1bce84bec
Fernando Suárez Jiménez <fsuarezj@gmail.com>
2025-03-26 14:27:43 +0300

    Deploy script

--- scripts/deploy_production.sh
+++ scripts/deploy_production.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
+
 # Upgrade your local PIP:
-$ python3.9 -m ensurepip --upgrade
-$ python3.9 -m pip install --upgrade pip
+python3.9 -m ensurepip --upgrade
+python3.9 -m pip install --upgrade pip
 
 # Make sure that you have your ~/.local/bin early in your path:
-$ cat ~/.bash_profile 
+cat ~/.bash_profile 
 export PATH=$HOME/.local/bin:$PATH
-$ source ~/.bash_profile
+source ~/.bash_profile
 
 echo @PATH > tmp.txt
