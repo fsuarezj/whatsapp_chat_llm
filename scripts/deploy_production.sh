901940ee98b04bf0cbb8b37d489e78350acb09fb
Fernando Suárez Jiménez <fsuarezj@gmail.com>
2025-03-26 14:26:42 +0300

    First commit

---
+++ scripts/deploy_production.sh
@@ -0,0 +1,10 @@
+# Upgrade your local PIP:
+$ python3.9 -m ensurepip --upgrade
+$ python3.9 -m pip install --upgrade pip
+
+# Make sure that you have your ~/.local/bin early in your path:
+$ cat ~/.bash_profile 
+export PATH=$HOME/.local/bin:$PATH
+$ source ~/.bash_profile
+
+echo @PATH > tmp.txt
