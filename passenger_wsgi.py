d6669426a5cb45556b538851eeab6b40a11d1fcf
Fernando Suárez Jiménez <fsuarezj@gmail.com>
2025-03-26 22:23:05 +0300

    Test passenger

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,15 +1,18 @@
 from dotenv import load_dotenv
-
 import sys
 import os
 
-load_dotenv()
-print(os.getenv('HOME'))
-INTERP = os.path.join(os.getenv('HOME'), 'whatsapp-chat-llm.xastrin.com/.venv/bin/python')
-if sys.executable != INTERP:
-    os.execl(INTERP, INTERP, *sys.argv)
+def application(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return [b'Hello, World!']
 
-from src.app import app as application
+#load_dotenv()
+#print(os.getenv('HOME'))
+#INTERP = os.path.join(os.getenv('HOME'), 'whatsapp-chat-llm.xastrin.com/.venv/bin/python')
+#if sys.executable != INTERP:
+#    os.execl(INTERP, INTERP, *sys.argv)
+#
+#from src.app import app as application
 
 #if __name__ == '__main__':
 #    application.run() 
