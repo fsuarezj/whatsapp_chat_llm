3210d2bb041bf8193f1a57bd5f1c752b00bf0540
Fernando Suárez Jiménez <fsuarezj@gmail.com>
2025-05-02 01:24:12 +0300

    Fixed conf

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -11,21 +11,4 @@ sys.path.insert(0, os.path.join(os.getcwd(), ApplicationDirectory))
 sys.path.insert(0, os.path.join(os.getcwd(), VirtualEnvDirectory, 'bin'))
 os.chdir(os.path.join(os.getcwd(), ApplicationDirectory))
 #os.environ.setdefault('DJANGO_SETTINGS_MODULE', ApplicationName + '.settings')
-
-def app(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    return [b'Hello, World!']
-
-application = app
-
-#from dotenv import load_dotenv
-#load_dotenv()
-#print(os.getenv('HOME'))
-#INTERP = os.path.join(os.getenv('HOME'), 'whatsapp-chat-llm.xastrin.com/.venv/bin/python')
-#if sys.executable != INTERP:
-#    os.execl(INTERP, INTERP, *sys.argv)
-#
-#from src.app import app as application
-
-#if __name__ == '__main__':
-#    application.run() 
+from whatsapp-chat-llm.src.app import app as application
