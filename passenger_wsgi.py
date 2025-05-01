d8d68ced34ffc7ee1fd03c81d6126ac50cb69e1e
Fernando Suárez Jiménez <fsuarezj@gmail.com>
2025-05-02 01:52:09 +0300

    Fixed conf

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -8,9 +8,10 @@ if sys.executable != VirtualEnv: os.execl(VirtualEnv, VirtualEnv, *sys.argv)
 sys.path.insert(0, os.path.join(os.getcwd(), ApplicationDirectory))
 sys.path.insert(0, os.path.join(os.getcwd(), VirtualEnvDirectory, 'bin'))
 os.chdir(os.path.join(os.getcwd(), ApplicationDirectory))
+from wcl.src.app import app as application
 
-def app(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    return [b'Hello, World!']
-
-application = app
+#def app(environ, start_response):
+#    start_response('200 OK', [('Content-Type', 'text/plain')])
+#    return [b'Hello, World!']
+#
+#application = app
