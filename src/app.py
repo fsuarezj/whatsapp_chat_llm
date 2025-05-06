70d3120043f3667b54c8b46b17d55b3a118baeb1
Fernando Suárez Jiménez <fsuarezj@gmail.com>
2025-05-06 09:52:39 +0300

    Improved tests

--- src/app.py
+++ src/app.py
@@ -20,7 +20,7 @@ dotenv.load_dotenv()
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-    app.config['SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+    app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
     db.init_app(app)
     # Register blueprints
 #    app.register_blueprint(auth_bp)
---
+++ src/tests/conftest.py
@@ -0,0 +1,20 @@
+import pytest
+from flask import Flask
+from models import db
+import os
+
+@pytest.fixture
+def app():
+    app = Flask(__name__)
+    app.config['TESTING'] = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+        yield app
+        db.session.remove()
+        db.drop_all()
